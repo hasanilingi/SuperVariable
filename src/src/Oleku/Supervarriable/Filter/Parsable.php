90bcaacdcf47fcb78367e330cc549e43e0861bd0
Hasan Ilingi <hasanilingi@gmail.com>
2013-08-21 16:14:23 +0300

    Changed for packagist package

---
+++ src/src/Oleku/Supervarriable/Filter/Parsable.php
@@ -0,0 +1,15 @@
+<?php namespace Oleku\Supervarriable\Filter;
+
+/**
+ * @author  Oleku Konko
+ * @license http://opensource.org/licenses/MIT
+ * @link    https://github.com/olekukonko/SuperVariable/
+ */
+
+
+interface Parsable
+{
+	public function parse ( $key, $mixed );
+}
+
+?>
